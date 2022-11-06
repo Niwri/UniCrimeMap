7b1aa76a540813a74839248929886cb32309dbdc
aravind987 <50051060+aravind987@users.noreply.github.com>
2022-11-05 21:02:43 -0400

    create data file

--- uni-crime-project/src/map_scripts/mapPage.js
+++ uni-crime-project/src/map_scripts/mapPage.js
@@ -8,7 +8,6 @@ import { Loader } from "@googlemaps/js-api-loader"
 function MapPage() {
     let map;
     const additionalOptions = {};
-    
     const loader = new Loader({
         apiKey: "AIzaSyBYOQr_EjZiS-CV1AuLighoZ_Sr_ZGWFto",
         version: "weekly",
@@ -17,8 +16,8 @@ function MapPage() {
       
     loader.load().then((google) => {
     map = new google.maps.Map(document.getElementById("map"), {
-        center: { lat: -34.397, lng: 150.644 },
-        zoom: 8,
+        center: { lat: 43.663744035827754, lng: -79.39479158104825 },
+        zoom: 16,
     });
     });
 }
